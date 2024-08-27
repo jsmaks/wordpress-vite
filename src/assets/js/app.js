f52b6881c05862855256a44265177eb842bbeb29
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-08-27 18:27:05 +0300

    feat: add section plans

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,9 +1,15 @@
-import '../scss/main.scss';
-const modules = import.meta.glob('./*.js', { eager: true });
+// import '../scss/main.scss';
+// const modules = import.meta.glob('./*.js', { eager: true });
 
-// Логика для каждого модуля (если необходимо)
-Object.values(modules).forEach(module => {
-  if (typeof module.default === 'function') {
-    module.default();
-  }
-});
+// // Логика для каждого модуля (если необходимо)
+// Object.values(modules).forEach(module => {
+//   if (typeof module.default === 'function') {
+//     module.default();
+//   }
+// });
+
+import '../scss/main.scss';
+
+const modules = import.meta.glob(['@/assets/js/*.js', '@/assets/js/*.ts']);
+
+Object.values(modules).map(module => module());
